500739893a3528c3605394b063586d530574cfc2
Rachel <32782380+elra2@users.noreply.github.com>
2017-11-10 17:07:28 +0000

    Update xboxcont_mbed.cpp

--- xboxcont_mbed.cpp
+++ xboxcont_mbed.cpp
@@ -5,18 +5,16 @@
 
 Serial pc(USBTX, USBRX);
 C12832 lcd(p5, p7, p6, p8, p11);
-
-/* Code output indicators/verifications; to be disabled or abled when LCD not available
+/* --- Code output/verifitcation indicators in place of lcd should it be needed
 DigitalOut forward(LED1);
 DigitalOut back(LED2);
 DigitalOut left(LED3);
-DigitalOut right(LED4); 
----*/
-
+DigitalOut right(LED4);
+---- */
 
 //Servo
-Servo servo_L(p22);
-Servo servo_R(p21);
+Servo servo_L(p22); //CH1 WIRE
+Servo servo_R(p21); //CH2 WIRE
 
 //Fastest = 3, Slowest = 1
 float MOVEBACK_SPEED3 = 0.8;
@@ -33,82 +31,112 @@ float MOVEFORWARD_SPEED3 = 0.2;
 //New xbox controller
 USBHostXpad XBOXCONT;
 
-void onXpadEvent (int buttons, int stick_lx, int stick_ly, int stick_rx, int stick_ry, int trigger_l, int trigger_r) {
-         //STICK POSITION
+void onXpadEvent (int buttons, int stick_lx, int stick_ly, int stick_rx, int stick_ry, int trigger_l, int trigger_r) {            
+         //STICK POSITION - LEFT
          if(stick_lx < -16000) {
             lcd.cls();
             lcd.printf("Left\n");
             lcd.cls();
             lcd.locate(0,3);
-            servo_L = MOVEBACK_SPEED3;
-            servo_R = MOVEFORWARD_SPEED3;
-            Thread::wait(300);
+            servo_L = MOVEBACK_SPEED2;
+            servo_R = MOVEFORWARD_SPEED2;
         }
         else if(stick_lx > 16000) {
             lcd.cls();
             lcd.printf("Right\n");
             lcd.cls();
             lcd.locate(0,3);
-            servo_L = MOVEFORWARD_SPEED3;
-            servo_R = MOVEBACK_SPEED3;
-            Thread::wait(300);
+            servo_L = MOVEFORWARD_SPEED2;
+            servo_R = MOVEBACK_SPEED2;
             }
         else if(stick_ly < -16000) {
             lcd.cls();
             lcd.printf("Backwards\n");
             lcd.cls();
             lcd.locate(0,3);
-            servo_L = MOVEBACK_SPEED3;
-            servo_R = MOVEBACK_SPEED3;
-            Thread::wait(300);
+            servo_L = MOVEBACK_SPEED2;
+            servo_R = MOVEBACK_SPEED2;
         }
         else if(stick_ly > 16000) {
             lcd.cls();
             lcd.printf("Forward\n");
             lcd.cls();
             lcd.locate(0,3);
-            servo_L = MOVEFORWARD_SPEED3;
-            servo_R = MOVEFORWARD_SPEED3;
-            Thread::wait(300);
+            servo_L = MOVEFORWARD_SPEED2;
+            servo_R = MOVEFORWARD_SPEED2;
+            }
+            
+      //STICK POSITION - RIGHT
+         if(stick_lx < -16000) {
+            lcd.cls();
+            lcd.printf("Left\n");
+            lcd.cls();
+            lcd.locate(0,3);
+            servo_L = MOVEBACK_SPEED2;
+            servo_R = MOVEFORWARD_SPEED2;
+        }
+        else if(stick_lx > 16000) {
+            lcd.cls();
+            lcd.printf("Right\n");
+            lcd.cls();
+            lcd.locate(0,3);
+            servo_L = MOVEFORWARD_SPEED2;
+            servo_R = MOVEBACK_SPEED2;
+            }
+        else if(stick_ly < -16000) {
+            lcd.cls();
+            lcd.printf("Backwards\n");
+            lcd.cls();
+            lcd.locate(0,3);
+            servo_L = MOVEBACK_SPEED2;
+            servo_R = MOVEBACK_SPEED2;
+        }
+        else if(stick_ly > 16000) {
+            lcd.cls();
+            lcd.printf("Forward\n");
+            lcd.cls();
+            lcd.locate(0,3);
+            servo_L = MOVEFORWARD_SPEED2;
+            servo_R = MOVEFORWARD_SPEED2;
             }
             
             
         //ABXY BUTTONS    
-        else if(XBOXCONT.read(USBHostXpad::XPAD_PAD_A) == 1){
+        if(XBOXCONT.read(USBHostXpad::XPAD_PAD_A) == 1){
         lcd.cls();
-        lcd.printf("BUTTON A PRESSED", USBHostXpad::XPAD_PAD_A);
+        lcd.printf("BUTTON A PRESSED");
         servo_L = STANDSTILL;
         servo_R = STANDSTILL;
             }
        else if(XBOXCONT.read(USBHostXpad::XPAD_PAD_B) == 1){
             lcd.cls(); 
-            lcd.printf("BUTTON B PRESSED", USBHostXpad::XPAD_PAD_B);
+            lcd.printf("BUTTON B PRESSED");
             }
         else if(XBOXCONT.read(USBHostXpad::XPAD_PAD_X) == 1){
             lcd.cls();
-            lcd.printf("BUTTON X PRESSED", USBHostXpad::XPAD_PAD_X);
+            lcd.printf("BUTTON X PRESSED");
             }
        else if(XBOXCONT.read(USBHostXpad::XPAD_PAD_Y) == 1){
             lcd.cls();
-            lcd.printf("BUTTON Y PRESSED", USBHostXpad::XPAD_PAD_Y);
+            lcd.printf("BUTTON Y PRESSED");
             }
             
         //LEFT TRIGGER
-       else if(XBOXCONT.read(USBHostXpad::XPAD_TRIGGER_L) >= 0.1 && XBOXCONT.read(USBHostXpad::XPAD_TRIGGER_L) <= 0.3 ){
+       while(XBOXCONT.read(USBHostXpad::XPAD_TRIGGER_L) >= 0.1 && XBOXCONT.read(USBHostXpad::XPAD_TRIGGER_L) <= 0.3 ){
             lcd.cls();
             lcd.printf("LOW SPEED", USBHostXpad::XPAD_PAD_Y);
             lcd.locate(0,3);
             servo_L = MOVEBACK_SPEED1;
             servo_R = MOVEBACK_SPEED1;
             }
-       else if(XBOXCONT.read(USBHostXpad::XPAD_TRIGGER_L) >= 0.4 && XBOXCONT.read(USBHostXpad::XPAD_TRIGGER_L) <= 0.7 ){
+       while(XBOXCONT.read(USBHostXpad::XPAD_TRIGGER_L) >= 0.4 && XBOXCONT.read(USBHostXpad::XPAD_TRIGGER_L) <= 0.7 ){
             lcd.cls();
             lcd.printf("HIGHER SPEED", USBHostXpad::XPAD_PAD_Y);
             lcd.locate(0,3);
             servo_L = MOVEBACK_SPEED2;
             servo_R = MOVEBACK_SPEED2;
             }
-       else if(XBOXCONT.read(USBHostXpad::XPAD_TRIGGER_L) >= 0.8 && XBOXCONT.read(USBHostXpad::XPAD_TRIGGER_L) <= 1 ){
+       while(XBOXCONT.read(USBHostXpad::XPAD_TRIGGER_L) >= 0.8 && XBOXCONT.read(USBHostXpad::XPAD_TRIGGER_L) <= 1 ){
             lcd.cls();
             lcd.printf("FASTEST SPEED", USBHostXpad::XPAD_PAD_Y);
             lcd.locate(0,3);
@@ -117,30 +145,56 @@ void onXpadEvent (int buttons, int stick_lx, int stick_ly, int stick_rx, int sti
             }
         
         //RIGHT TRIGGER
-       else if(XBOXCONT.read(USBHostXpad::XPAD_TRIGGER_R) >= 0.1 && XBOXCONT.read(USBHostXpad::XPAD_TRIGGER_R) <= 0.3 ){
+      while(XBOXCONT.read(USBHostXpad::XPAD_TRIGGER_R) >= 0.1 && XBOXCONT.read(USBHostXpad::XPAD_TRIGGER_R) <= 0.3 ){
             lcd.cls();
             lcd.printf("LOW SPEED", USBHostXpad::XPAD_PAD_Y);
             lcd.locate(0,3);
             servo_L = MOVEFORWARD_SPEED1;
             servo_R = MOVEFORWARD_SPEED1;
             }
-       else if(XBOXCONT.read(USBHostXpad::XPAD_TRIGGER_R) >= 0.4 && XBOXCONT.read(USBHostXpad::XPAD_TRIGGER_R) <= 0.7 ){
+       while(XBOXCONT.read(USBHostXpad::XPAD_TRIGGER_R) >= 0.4 && XBOXCONT.read(USBHostXpad::XPAD_TRIGGER_R) <= 0.7 ){
             lcd.cls();
             lcd.printf("HIGHER SPEED", USBHostXpad::XPAD_PAD_Y);
             lcd.locate(0,3);
             servo_L = MOVEFORWARD_SPEED2;
             servo_R = MOVEFORWARD_SPEED2;
             }
-       else if(XBOXCONT.read(USBHostXpad::XPAD_TRIGGER_R) >= 0.8 && XBOXCONT.read(USBHostXpad::XPAD_TRIGGER_R) <= 1 ){
+       while(XBOXCONT.read(USBHostXpad::XPAD_TRIGGER_R) >= 0.8 && XBOXCONT.read(USBHostXpad::XPAD_TRIGGER_R) <= 1 ){
             lcd.cls();
             lcd.printf("FASTEST SPEED", USBHostXpad::XPAD_PAD_Y);
             lcd.locate(0,3);
             servo_L = MOVEFORWARD_SPEED3;
             servo_R = MOVEFORWARD_SPEED3;
             }
-        else {
-            servo_L = STANDSTILL;
-            servo_R = STANDSTILL;
+            
+      //DPAD POSITION
+        if(XBOXCONT.read(USBHostXpad::XPAD_HAT_UP) == 1){
+            lcd.cls(); 
+            lcd.printf("forward %f", USBHostXpad::XPAD_HAT_UP);
+            lcd.locate(0,3);
+            servo_L = MOVEFORWARD_SPEED1;
+            servo_R = MOVEFORWARD_SPEED1;
+            }
+        else if(XBOXCONT.read(USBHostXpad::XPAD_HAT_LEFT) == 1){
+            lcd.cls(); 
+            lcd.printf("LEFT %f", USBHostXpad::XPAD_HAT_LEFT);
+            lcd.locate(0,3);
+            servo_L = MOVEBACK_SPEED1;
+            servo_R = MOVEFORWARD_SPEED1;
+            }
+        else if(XBOXCONT.read(USBHostXpad::XPAD_HAT_RIGHT) == 1){
+            lcd.cls(); 
+            lcd.printf("RIGHT %f", USBHostXpad::XPAD_HAT_RIGHT);
+            lcd.locate(0,3);
+            servo_L = MOVEFORWARD_SPEED1;
+            servo_R = MOVEBACK_SPEED1;
+            }
+        else if(XBOXCONT.read(USBHostXpad::XPAD_HAT_DOWN) == 1){
+            lcd.cls(); 
+            lcd.printf("DOWN %f", USBHostXpad::XPAD_HAT_DOWN);
+            lcd.locate(0,3);
+            servo_L = MOVEBACK_SPEED1;
+            servo_R = MOVEBACK_SPEED1;
             }
             
 volatile int poll = 0;
@@ -150,9 +204,9 @@ void xpad_task(void const *) {
     volatile int poll = 0;
     lcd.cls();
     lcd.printf("Disconnected..");
-    right = 1;
     while(1) {
         // try to connect a Xbox 360 Wireless Controller
+        
         while(!XBOXCONT.connect())
             Thread::wait(500);
         // when connected, attach handler called on xpad event
@@ -169,7 +223,6 @@ void xpad_task(void const *) {
         XBOXCONT.led(USBHostXpad::LED2_ON);
         lcd.cls();
         lcd.printf("Connected!");
-        left = 1;
         // wait until the mouse is disconnected
         while(XBOXCONT.connected()) {
             Thread::wait(500);
@@ -188,6 +241,10 @@ int main() {
     pc.baud(115200);
     lcd.cls();
     while(1) {
-        Thread::wait(200);
+           //INITIAL POSITION
+            servo_L = STANDSTILL;
+            servo_R = STANDSTILL;
+            lcd.printf("Still Standing");
+        Thread::wait(500);
     }
 }
